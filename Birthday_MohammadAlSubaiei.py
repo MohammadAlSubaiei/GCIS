c74f40f77870d362d5117193f833e83a36d74f4f
MohammadAlSubaiei <156885902+MohammadAlSubaiei@users.noreply.github.com>
2024-01-22 11:14:31 +0400

    Add files via upload

---
+++ Birthday_MohammadAlSubaiei.py
@@ -0,0 +1,10 @@
+
+
+
+name = input ("Type your name :")
+birthMonth = input ("Type the birth month in text :") 
+birthDay = int(input ("Type the birth day in numbers :"))
+birthyear = int(input("Type the birth year in numbers :"))
+
+print (name, "your birthday is on", birthMonth, birthDay, birthyear)
+
